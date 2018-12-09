a29a08c8039d2a71fd2119ee16ec00d3c0cec085
Jason Yeh <jason@yehs.us>
2018-12-08 20:22:38 -0500

    improve unit tests

--- test/integration/users.test.js
+++ test/integration/users.test.js
@@ -3,7 +3,7 @@ const request = require('supertest')
 const app = require('../../src/app')
 const db = require('../../src/db')
 
-describe('Users', () => {
+describe('User API Integration Tests', () => {
   beforeEach(async () => {
     await db.migrate.rollback()
     await db.migrate.latest()
@@ -19,15 +19,35 @@ describe('Users', () => {
     await db.destroy()
   })
 
+  describe('Fail to login', () => {
+    it('<401 Forbidden> should always return when no authenticated user', async () => {
+      const res = await request(app.callback())
+        .post('/api/v1/user')
+        .send({ email: 'johndoe@example.com', password: 'test' })
+        .expect('Content-Type', /json/)
+        .expect(405)
+
+      const { error } = res.body
+      expect(error).toBe('Method Not Allowed')
+    })
+  })
+
   describe('Create User', () => {
     it('<200> should always return with the API server information', async () => {
       const res = await request(app.callback())
-        .get('/api/v1/ping')
+        .post('/api/v1/user/register')
+        .send({
+          email: 'johndoe@example.com',
+          password: 'test',
+          firstName: 'John',
+          lastName: 'Doe'
+        })
         .expect('Content-Type', /json/)
         .expect(200)
 
-      const { healthy } = res.body
-      expect(healthy).toBe(true)
+      console.log(res.body)
+      // const { healthy } = res.body
+      // expect(healthy).toBe(true)
     })
   })
 })
